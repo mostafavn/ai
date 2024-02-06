f24cd6f4f02fdae2995d3c45e06bcb7bc4beb429
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2024-02-06 22:42:27 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+BWhite='\033[1;37m' 
+NC='\033[0m'
+
+echo -e "${BWhite}Installing . . .${NC}"
 apt install python3 python3-pip wget > /dev/null 2>&1
 pip3 install --upgrade openai  > /dev/null 2>&1
 curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai  > /dev/null 2>&1
+mv ./ai /usr/bin
+chmod +x /usr/bin/ai
+rm ./install.sh
+clear
+echo -e "${BWhite}Installed .${NC}"
