782cec72dceb30b0fcbd1a1148bd60bbdb3c3e9e
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2024-02-06 23:57:43 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ pip3 install termcolor > /dev/null 2>&1
 curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai > /dev/null 2>&1
 mv ./ai /usr/bin
 chmod +x /usr/bin/ai
-rm ./install.sh
 curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai.1.gz > /dev/null 2>&1
 mv ai.1.gz /usr/share/man/man1
 clear
 echo -e "${BWhite} Successfully installed [man ai]${NC}"
+rm ./install.sh
