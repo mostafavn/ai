697ce6bb4599cb4da01d7a231c689200ec35e5a0
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2024-02-06 23:18:45 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai  > /dev/null 2>&
 mv ./ai /usr/bin
 chmod +x /usr/bin/ai
 rm ./install.sh
-curl -O https://github.com/mostafa-vn/ai/blob/main/ai.4.gz  > /dev/null 2>&1
+curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai.4.gz  > /dev/null 2>&1
 mv ai.4.gz /usr/share/man/man4
 clear
 echo -e "${BWhite} Successfully installed [man ai]${NC}"
