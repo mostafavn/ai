c638296266e692c5c9da609a0431af93b84b0c2a
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2024-02-06 21:35:27 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+if ! command -v python3 &> /dev/null
+then
+    read -p "python nasb she? " confirm
+    if [[ $confirm == [yY] || ]]
+    then
+        apt install python3 python3-pip wget
+    fi
+fi
 apt install python3 python3-pip wget
 pip3 install --upgrade openai
 curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai
