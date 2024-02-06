905dc033f64226eb9751ac2caca18cfb59bd1753
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2024-02-06 23:34:55 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@ NC='\033[0m'
 clear
 echo -e "${BWhite} Installing... ${NC}"
 apt install python3 python3-pip wget > /dev/null 2>&1
-pip3 install --upgrade openai  > /dev/null 2>&1
-curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai  > /dev/null 2>&1
+pip3 install --upgrade openai > /dev/null 2>&1
+pip3 install termcolor > /dev/null 2>&1
+curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai > /dev/null 2>&1
 mv ./ai /usr/bin
 chmod +x /usr/bin/ai
 rm ./install.sh
-curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai.1.gz  > /dev/null 2>&1
+curl -O https://raw.githubusercontent.com/mostafa-vn/ai/main/ai.1.gz > /dev/null 2>&1
 mv ai.1.gz /usr/share/man/man1
 clear
 echo -e "${BWhite} Successfully installed [man ai]${NC}"
