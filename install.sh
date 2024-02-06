98a33e469c0b487a446357873a7fca9b9c95beeb
Mostafa VN <mostafa.vahedi.nejad@gmail.com>
2024-02-06 21:15:25 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 apt install python3 python3-pip wget
+
+echo '1'
+
 pip3 install --upgrade openai
+
+echo '2'
+
 wget https://https://raw.githubusercontent.com/mostafa-vn/ai/main/ai
+
+echo '3'
+
 mv ./ai /usr/bin
+
+echo '4'
+
 chmod +x /usr/bin/ai
